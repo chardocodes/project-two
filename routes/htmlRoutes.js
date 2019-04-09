885121aa3597d915832bea06def38e61adfd79d1
Manny <manueltrujillo_02@hotmail.com>
2019-04-08 20:59:05 -0700

    i got the routing done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,9 @@ const db = require("../models");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = app => {
   // Load signup page
-  app.get("/", (req, res) => res.render("example"));
+  app.get("/", (req, res) => res.render("dogwalkers"));
   app.get("/signup", (req, res) => res.render("signup"));
+  app.get("/profile", (req, res) => res.render("profile"));
   app.get("/dogwalkers", (req, res) => res.render("dogwalkers"));
 
   // Load login page

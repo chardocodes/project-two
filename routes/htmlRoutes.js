19566992d99b7d394feb92c17d51dc8c95376c32
Manny <manueltrujillo_02@hotmail.com>
2019-04-09 13:11:50 -0700

    manny

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,24 +4,20 @@ module.exports = app => {
   // Load signup page
   app.get("/", (req, res) => res.render("dogwalkers"));
   app.get("/signup", (req, res) => res.render("signup"));
-  app.get("/profile", (req, res) => res.render("profile"));
-  app.get("/dogwalkers", (req, res) => res.render("dogwalkers"));
+ 
+ 
+
 
   // Load login page
-  app.get("/login", (req, res) => res.render("login"));
+
 
   // Load profile page
-  app.get("/profile", isAuthenticated, (req, res) => {
-    db.User.findOne({
-      where: {
-        id: req.user.id
-      },
-      include: [db.Example]
-    }).then(dbUser => {
+  app.get("/profile", (req, res) => {
+    db.User.findall()
       res.render("profile", { user: dbUser });
-    });
+ 
   });
-
+  app.get("/login", (req, res) => res.render("login"));
   // Load example page and pass in an example by id
   app.get("/example/:id", isAuthenticated, (req, res) => {
     db.Example.findOne({ where: { id: req.params.id } }).then(dbExample => {
